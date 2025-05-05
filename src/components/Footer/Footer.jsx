68d6bc18a6fb8405d33b1d490ca9072a44dfe989
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-05-05 16:08:54 +0600

    add logo slider with react-infinite-logo-slider

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="Footer-container"></div>;
+  return <div className="Footer-container mb-40"></div>;
 }
 
 export default Footer;
--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Logos.css";
+import Myslider from "../Slider/Slider";
 
-function Logos() {
-  return <div className="Logos-container"></div>;
-}
+const Logos = () => {
+  return (
+    <section className="overflow-hidden px-[33px] py-6 relative content-center items-center h-[82px]">
+      <Myslider />
+      <div className="absolute left-0 top-0 bg-gradient-to-r to-transparent from-[rgb(8,8,8)] w-[50%] h-full"></div>
+      <div className="absolute right-0 top-0 bg-gradient-to-l to-transparent from-[rgb(8,8,8)] w-[50%] h-full"></div>
+    </section>
+  );
+};
 
 export default Logos;
