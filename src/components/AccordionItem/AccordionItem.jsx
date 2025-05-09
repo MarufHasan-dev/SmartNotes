2054fa0df97e3459dea101ed10d9768de68cbd75
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-05-09 11:26:33 +0600

    add animation to FAQ section

--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -21,18 +21,24 @@ function AccordionItem({ item, openQuestion, setOpenQuestion }) {
           {question}
         </p>
         <img
-          className="border-secondary-200/70 w-[32px] h-[32px] border rounded-lg"
+          className={`border-secondary-200/70 w-[32px] h-[32px] border rounded-lg transition-transform duration-300 ${
+            open && `rotate-90`
+          }`}
           src={arrow}
           alt="arrow icon"
         />
       </button>
-      {open ? (
-        <div className="flex flex-row mt-5 bg-accent border-1 border-secondary-200/60 rounded-2xl gap-6.5 p-7.5">
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${
+          open ? `max-h-50 opacity-100 mt-5` : `max-h-0 opacity-0 mt-0`
+        }`}
+      >
+        <div className="flex flex-row bg-accent border-1 border-secondary-200/60 rounded-2xl gap-6.5 p-7.5">
           <div className="flex w-full gap-5 flex-col">
             <p className="font-[DM_Sans] text-xl leading-[140%] text-primary-400 font-bold">
               {answerHeading}
             </p>
-            <p className="font-[DM_Sans] text-base text-[#333333] leading-[150%]">
+            <p className="font-[DM_Sans] text-base text-[#333333] leading-[150%] text-justify">
               {answer}
             </p>
           </div>
@@ -42,9 +48,7 @@ function AccordionItem({ item, openQuestion, setOpenQuestion }) {
             alt="arrow icon"
           />
         </div>
-      ) : (
-        ""
-      )}
+      </div>
     </div>
   );
 }
